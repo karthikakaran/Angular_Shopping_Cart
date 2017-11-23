e82224b9c29115801353b0f2a70f47c73a4a8d3a
Karthika Karunakaran <karthika.karunakaran28@gmail.com>
2017-11-24 00:08:11 +0530

    Add files via upload

---
+++ controller.js
@@ -0,0 +1,78 @@
+var myapp = angular.module('myapp', ['ngRoute']);
+myapp.config(['$routeProvider',function($routeProvider) {
+        $routeProvider
+            .when('/showProducts', {
+                templateUrl : 'shoppingCart.htm',
+                controller  : 'shoppingcartController'
+            })
+	    .when('/checkout', {
+                templateUrl : 'CheckoutCart.htm',
+                controller  : 'checkoutCartController'
+            })
+	    .otherwise({
+                redirectTo: '/'
+            });
+}]);
+
+myapp.controller('mainController', function($scope, $http, $window, $location) {
+	 $location.path("showProducts");
+});
+
+myapp.controller('shoppingcartController', function($scope, $http, $window, $location, $rootScope) {
+  var flag = true;
+  if(typeof JSON.stringify($rootScope.products) == 'string') flag = false;
+  if (flag) {
+    $http({
+      method: 'GET',
+      url: 'products.js'
+    }).then(function successCallback(response) {
+      $rootScope.products = response['data'];
+    }, function errorCallback(response) {
+      alert('Oops!');
+    });
+  }
+  $scope.values = {};
+  $scope.checkout = function(){
+    var values = JSON.stringify($scope.values);
+    $.each($scope.values, function(key, value) {
+      var price = $rootScope.products[key]['Price'];
+      $rootScope.products[key]['CostOfItem'] = value * price;
+      $rootScope.products[key]['Quantity'] = value;
+    });
+    if(JSON.stringify($scope.values) === JSON.stringify({})) alert('Please add items to the cart!');
+    else $location.path('checkout');
+  };
+
+});
+
+myapp.controller('checkoutCartController', function($scope, $http, $window, $location, $rootScope) {
+  $scope.totalAmt = 0;
+  $.each($rootScope.products, function (key, value) {
+    if ($scope.products[key]['CostOfItem'])
+      $scope.totalAmt += $scope.products[key]['CostOfItem'];
+  });
+
+  $scope.delete = function (index) {
+    $scope.totalAmt -= $scope.products[index]['CostOfItem']
+    delete $scope.products[index]['CostOfItem'];
+    delete $scope.products[index]['Quantity'];
+  }
+
+  $scope.changeQuantity = function (index) {
+     if($scope.products[index]['Quantity'] != null) {
+       $scope.totalAmt -= $scope.products[index]['CostOfItem'];
+       $scope.products[index]['CostOfItem'] = $scope.products[index]['Quantity'] * $scope.products[index]['Price'];
+       $scope.totalAmt += $scope.products[index]['CostOfItem'];
+     }
+  }
+
+  $scope.changeItemInCart = function() {
+    $rootScope.products = angular.copy($scope.products);
+ 	  $location.path('showProducts');
+  }
+
+  $scope.message = function() {
+    $scope.totalAmt == 0 ? alert('No items selected!') : alert('PAID! Thank You!');
+  }
+
+});
---
+++ products.js
@@ -0,0 +1,12 @@
+[
+    {"Item" : "rings1", "Name": "Halo", "Type" : "Classic-C905R1", "Price" : 25},
+    {"Item" : "rings2", "Name": "Gemstone", "Type" : "Classic-C905R2", "Price" : 90},
+    {"Item" : "rings3", "Name": "Cathedral", "Type" : "Classic-C905R3", "Price" : 15},
+    {"Item" : "rings4", "Name": "Vintage", "Type" : "Classic-C905R4", "Price" : 35},
+    {"Item" : "rings5", "Name": "Swirl", "Type" : "Classic-C905R5", "Price" : 95},
+    {"Item" : "rings6", "Name": "Prong", "Type" : "Classic-C905R6", "Price" : 45},
+    {"Item" : "rings7", "Name": "Princess", "Type" : "Classic-C905R7", "Price" : 15},
+    {"Item" : "rings8", "Name": "Pave", "Type" : "Classic-C905R8", "Price" : 20},
+    {"Item" : "rings9", "Name": "Cushion", "Type" : "Classic-C905R9", "Price" : 65},
+    {"Item" : "rings10", "Name": "Bezel", "Type" : "Classic-C905R10", "Price" : 15}
+  ]
